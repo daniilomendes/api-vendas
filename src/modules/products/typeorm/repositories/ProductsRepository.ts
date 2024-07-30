ebdeb071cf706972c4e85ff6be29d2c0e6cfc732
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-30 09:01:14 -0300

    feat: add corrections and products routes

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,7 +1,7 @@
 import { EntityRepository, Repository } from "typeorm";
 import Product from "../entities/Product";
 
-@EntityRepository()
+@EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
   public async findByName(name: string): Promise<Product | undefined> {
     const product = this.findOne({
