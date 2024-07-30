0b719f787b8aa305eabec7019c0729bd91e0de35
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-30 07:57:34 -0300

    feat: add CreateProductService service

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository } from "typeorm";
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+import { AppError } from "@shared/errors/AppError";
+import Product from "../typeorm/entities/Product";
+
+interface IRequeste {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class CreateProductService {
+  public async execute({ name, price, quantity }: IRequeste): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError("There is already one product with this name.");
+    }
+
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
