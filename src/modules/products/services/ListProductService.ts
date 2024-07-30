154b20edb4631a938fb442760f23bd384dbe361c
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-30 08:00:34 -0300

    feat: add ListProductService service

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,13 @@
+import { getCustomRepository } from "typeorm";
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+import Product from "../typeorm/entities/Product";
+
+export default class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const products = productsRepository.find();
+
+    return products;
+  }
+}
